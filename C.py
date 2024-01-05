05fca1cb0d6146f7869a252d6924c47c033636d8
xirtam-ch <751348449@qq.com>
2024-01-05 18:41:50 +0800

    添加保存路径

--- C.py
+++ C.py
@@ -5,3 +5,4 @@ class C:
     REPORT_PATH = os.path.join(os.getcwd(), 'reports/')
     DATA_PATH = os.path.join(os.getcwd(), 'data/')
     CACHE_PATH = os.path.join(os.getcwd(), 'cache/')
+    XLS_OUTPUT_PATH = os.path.expanduser("~") + '/Downloads/'
